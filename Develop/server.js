302e67c23fab61bdb2199ff6499397d65c735940
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-10-31 20:24:01 +0000

    add mongoose connect

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,3 @@
-//ccreate connections
-//express server running 
-//routes to be set up (folder maybe) (routes folder)
-
-//templates online//class exc
-
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -18,6 +12,8 @@ app.use(express.staatic("public"));
 
 mongoose.connect(proess.env.MONGODB_URI || 'mongodb:localhost/workout', {
     useNewUrlParser:true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
 //routes
