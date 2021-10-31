736331b87a34161fff16d2d29d5fac3af97ea3ec
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-10-31 23:43:18 +0000

    correct pathway bug

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const workout = require('../models/workout');
+const db = require('../models/workout');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
@@ -125,7 +125,7 @@ const workoutSeed = [
   },
 ];
 
-workout.deleteMany({})
+db.Workout.deleteMany({})
   .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then((data) => {
     console.log(data.result.n + ' records inserted!');
