75712da5cf87a16805fbffcbebe69259d1b89fec
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-10-31 20:00:16 +0000

    Set up server connections

--- Develop/server.js
+++ Develop/server.js
@@ -5,9 +5,24 @@
 //templates online//class exc
 
 const express = require("express");
-
 const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.staatic("public"));
 
 mongoose.connect(proess.env.MONGODB_URI || 'mongodb:localhost/workout', {
     useNewUrlParser:true,
+});
+
+//routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
 });
