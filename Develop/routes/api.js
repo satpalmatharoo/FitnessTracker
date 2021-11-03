6d56fdbc170c54bf7336c0b5e52a0b058a3fd4e0
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-03 22:41:26 +0000

    add post and get request

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,6 +2,7 @@
 //take from  api.js end routes all apis
 
 const router = require("express").Router();
+const { Router } = require("express");
 const workout = require("../models/workout.js");
 
 //Add exercises to the most recent workout plan.
@@ -17,15 +18,46 @@ router.get ("/workout", (req, res) =>
         res.json(dbExcerise);
     }).catch(err => {
         res.status(400).json(err);
-    })
+    }),
 
+router.put ("/workout + id", (req, res) => {
+  workout.findOneAndUpdate (
+    {"_id": req.params.id},
+    {$push: {"excerise":excerise}},
+    {new:true},
     
-//});
+  ]).then(dbExcerise => {
+    res.json(dbExcerise);
 
-//Add new exercises to a new workout plan
+  }).catch(err => {
+    res.status(400).json(err);
+  })
+}),
+
+router.post("/workout", (req, res) =>{
+  workout.create(req.body)
+  .then(dbExcerise => {
+    res.json(dbExcerise);
+  })
+}).catch(err => {
+  res.status(400).json(err);
+}),
+
+router.get("/workout/range", (req, res) => {
+  workout.aggregrate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "exercises.duration"},
+        totalWeight: {$sum: "$excercise.weight"},
+      },
+    }
+  ])
+
 
 
 //View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
 
 
-//View the total duration of each workout from the past seven workouts on the `stats` page.
+//View the total duration of each workout from the past seven workouts on the `stats` page
+
+module.exports = router;
