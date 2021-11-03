13b421fcddd160bd83a8ee74f233d42528da93ac
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-03 16:30:59 +0000

    add aggregrate route

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,4 +2,30 @@
 //take from  api.js end routes all apis
 
 const router = require("express").Router();
-const workout = require("../../models/workout");
+const workout = require("../models/workout.js");
+
+//Add exercises to the most recent workout plan.
+router.get ("/workout", (req, res) => 
+   workout.aggregrate ([
+        {
+          $addFields: {
+            totalDuration: { $sum: "$excerises.duration" } ,
+            totalWeight: { $sum: "$excerises.weight" },
+          }
+        }
+    ]).then(dbExcerise => {
+        res.json(dbExcerise);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+
+    
+//});
+
+//Add new exercises to a new workout plan
+
+
+//View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
+
+
+//View the total duration of each workout from the past seven workouts on the `stats` page.
