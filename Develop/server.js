4b381bd1f38259594695c094e4aa20ec27a85a3a
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2021-11-05 20:12:25 +0000

    amend mongoose command

--- Develop/server.js
+++ Develop/server.js
@@ -10,11 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    {
     useNewUrlParser:true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-});
+    useUnifiedTopology:true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    }
+);
 
 //routes
 app.use(require("./routes/api.js"));
